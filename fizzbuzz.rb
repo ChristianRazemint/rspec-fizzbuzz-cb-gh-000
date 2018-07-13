cee9a223e165f34cfcc22f23c5b206ac7e896626
Christian Martínez <jchristianmartinezg@gmail.com>
2018-07-13 08:46:20 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,9 +4,9 @@ def fizzbuzz(int)
     if int % 3 == 0 && int % 3 == 0
         "FizzBuzz"
     elsif int % 5 == 0
-      "Fizz"
-    elsif int % 3 == 0
       "Buzz"
+    elsif int % 3 == 0
+      "Fizz"
     else
       nil
     end
